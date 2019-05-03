964416f44a487aa16fcdcd1cf687a89c4d9dbf70
davidgitgood <40075224+davidgitgood@users.noreply.github.com>
2019-05-03 15:29:22 -0700

    Imeplement Queue

---
+++ W1D5/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+    def initialize
+        @arr = []
+    end
+
+    def enqueue(el)
+        @arr << el
+    end
+
+    def dequeue
+        @arr.shift
+    end
+
+    def peek
+        @arr.first
+    end
+end
